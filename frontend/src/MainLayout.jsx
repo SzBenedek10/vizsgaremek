698b96cd66a828d138b03b62c52d825e62e5234a
thay-hun <gibszjakab900@gmail.com>
2026-04-15 14:14:57 +0200

    hiba

--- frontend/src/MainLayout.jsx
+++ frontend/src/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar.jsx';
-import Footer from '../components/footer';
+import Footer from './components/footer';
 
 export default function MainLayout() {
   return (
